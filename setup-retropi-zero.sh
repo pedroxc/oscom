5064287dfea43f99c48a7ec86048a37eb2b90711
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-05 13:22:18 -0300

    setup

---
+++ setup-retropi-zero.sh
@@ -0,0 +1,17 @@
+echo "=== Instalando pacotes essenciais ==="
+sudo apt install -y build-essential git unzip joystick python3-pip curl wget dialog libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 screen tmux ranger mc
+
+echo "=== Instalando BASIC ==="
+sudo apt install -y freebasic bas
+
+echo "=== Instalando DOSBox ==="
+sudo apt install -y dosbox
+
+echo "=== Instalando openMSX ==="
+sudo apt install -y openmsx
+
+echo "=== Instalando VICE (Commodore 64) ==="
+sudo apt install -y vice
+
+echo "=== Instalando RetroArch ==="
+sudo apt install -y retroarch retroarch-assets retroarch-dbg
