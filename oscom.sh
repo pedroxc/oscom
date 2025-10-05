5a711e42c1c8bd8671c4b0eb3b84313b903152ad
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-05 14:23:27 -0300

    setup

--- oscom.sh
+++ oscom.sh
@@ -1,17 +1,3 @@
-sudo apt install -y git build-essential libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev
-
-# 2. Clone o repositório BBCSDL
-cd ~
-git clone https://github.com/rtrussell/BBCSDL.git
-
-# 3. Vá para o diretório e compile
-cd BBCSDL
+cd ~/BBCSDL
+sudo apt install build-essential libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev -y
 make
-
-# 4. Depois de compilar, deve gerar executável (ex: bbcsdl)
-# Verifique com:
-ls -l bin
-
-# 5. Rodar
-cd bin
-./bbcsdl
