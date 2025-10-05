8d86bddda220f615ba91ab5435453d57c158f935
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-05 14:14:26 -0300

    setup

--- oscom.sh
+++ oscom.sh
@@ -1,20 +1,17 @@
-# 1. Instalar dependências
-sudo apt update
-sudo apt install libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-ttf-2.0-0
+sudo apt install -y git build-essential libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev
 
-# 2. Crie uma pasta para o BBC BASIC
+# 2. Clone o repositório BBCSDL
 cd ~
-mkdir BBCBASIC
-cd BBCBASIC
+git clone https://github.com/rtrussell/BBCSDL.git
 
-# 3. Baixar o zip da versão Raspberry Pi
-# (você precisa achar o link mais recente; pode ser no site oficial ou no GitHub)
-wget https://www.bbcbasic.co.uk/Downloads/bbcrpi.zip  # exemplo fictício, ajuste o nome
+# 3. Vá para o diretório e compile
+cd BBCSDL
+make
 
-# 4. Extrair
-unzip bbcrpi.zip
+# 4. Depois de compilar, deve gerar executável (ex: bbcsdl)
+# Verifique com:
+ls -l bin
 
-# 5. Tornar executável e rodar
-cd extracted_folder  # a pasta resultante da extração
-chmod +x bbcsdl
+# 5. Rodar
+cd bin
 ./bbcsdl
