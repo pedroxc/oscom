23e12babc9079e89ebf92d05a979e3f871babf82
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-05 15:47:19 -0300

    setup

--- libs.sh
+++
@@ -1,1 +0,0 @@
-sudo apt install -y libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0
--- shortcut.sh
+++
@@ -1,2 +0,0 @@
-echo -e '#!/bin/bash\ncd ~/oscom && git pull && bash oscom.sh && cd ~' | sudo tee /usr/local/bin/oscom > /dev/null
-sudo chmod +x /usr/local/bin/oscom
