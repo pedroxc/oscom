037c8797ff3de0cee3ed8e3e955b209dfe067e34
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-05 17:53:25 -0300

    setup

--- oscom.sh
+++ oscom.sh
@@ -1,9 +1,1 @@
-cd ~
-rm -rf SmallBASIC
-sudo apt install -y build-essential git autoconf automake pkg-config libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-gfx-dev libfreetype6-dev libfontconfig1-dev
-git clone --recurse-submodules https://github.com/smallbasic/SmallBASIC.git
-cd SmallBASIC
-./autogen.sh
-./configure --enable-sdl2
-make
-sudo make install
+sudo apt install fmsx
