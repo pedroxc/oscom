e3f165a1975e4102fcf362968f5716c56b4069ff
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-05 17:06:04 -0300

    setup

--- oscom.sh
+++ oscom.sh
@@ -1,6 +1,9 @@
-cd ~/SmallBASIC
-git submodule update --init --recursive
-make clean
+cd ~
+rm -rf SmallBASIC
+sudo apt install -y build-essential git autoconf automake pkg-config libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-gfx-dev libfreetype6-dev libfontconfig1-dev
+git clone --recurse-submodules https://github.com/smallbasic/SmallBASIC.git
+cd SmallBASIC
+./autogen.sh
 ./configure --enable-sdl2
 make
 sudo make install
