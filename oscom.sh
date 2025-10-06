d0f69f935af1829b507b108876c17be38f249c0d
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-06 19:48:02 -0300

    all

--- oscom.sh
+++ oscom.sh
@@ -1,7 +1,7 @@
 cd ~
-rm -rf blueberryMSX
-git clone --branch rpi1 https://github.com/0xe1f/blueberryMSX.git
+git clone https://github.com/0xe1f/blueberryMSX.git
 cd blueberryMSX
-sudo apt update
+git checkout 7e021f6   # commit antigo estável antes das mudanças SDL2
 sudo apt install libsdl1.2-dev zlib1g-dev build-essential
+make clean
 make RPI1=1
