cf93140458848bb113878a2b207edcd37208960d
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-06 19:49:59 -0300

    all

--- oscom.sh
+++ oscom.sh
@@ -1,7 +1,3 @@
-cd ~
-git clone https://github.com/0xe1f/blueberryMSX.git
-cd blueberryMSX
-git checkout 7e021f6   # commit antigo estável antes das mudanças SDL2
-sudo apt install libsdl1.2-dev zlib1g-dev build-essential
-make clean
-make RPI1=1
+sudo apt update
+sudo apt install fmsx
+sudo apt install openmsx
