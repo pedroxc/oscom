db43843594328b87b7cf429c475fd6506db60d4b
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-08 23:27:30 -0300

    setup

--- oscom.sh
+++ oscom.sh
@@ -1,5 +1,2 @@
-sudo install -d -o lightdm -g lightdm /var/lib/lightdm/data
-sudo apt update
-sudo apt install --no-install-recommends \
-  xserver-xorg lightdm raspberrypi-ui-mods pi-greeter \
-  lxsession openbox rpd-icons rpd-plym-splash
+sudo apt udpate
+sudo apt upgrade -y
