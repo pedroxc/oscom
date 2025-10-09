958ba39962319d8efd92a04b00d84acf4acc0e87
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-09 17:35:58 -0300

    setup

--- oscom.sh
+++ oscom.sh
@@ -1,12 +1,1 @@
-sudo apt update
-sudo apt install -y dosfstools mtools
-mkfs.fat -C -F 12 -n MSXDISK ~/RetroPie/roms/msx/meujogo.dsk 720
-mkdir -p ~/msx_dsk
-sudo mount -o loop,uid=$USER,gid=$USER ~/RetroPie/roms/msx/meujogo.dsk ~/msx_dsk
-
-# copie seus arquivos .BAS/.BIN/.COM etc para dentro do disco
-cp -v ~/RetroPie/roms/msx/SEUARQ.BAS ~/msx_dsk/
-# (repita para os outros)
-
-sync
-sudo umount ~/msx_dsk
+file ~/RetroPie/roms/msx/meudisco.dsk
