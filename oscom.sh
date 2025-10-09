f6665336a047129533c2fdde36eff14835c29b24
PedroXC <pedroxaviercoelho@gmail.com>
2025-10-09 16:22:07 -0300

    setup

--- oscom.sh
+++ oscom.sh
@@ -1,15 +1,1 @@
-echo "pedro -2"
-
-sudo mkdir -p /opt/retropie/configs/all/retroarch/config/blueMSX
-sudo tee /opt/retropie/configs/all/retroarch/config/blueMSX/blueMSX.cfg >/dev/null <<'INI'
-# Exigir tecla modificadora: SEM ela nada dispara
-input_enable_hotkey = "rctrl"
-
-# Agora os atalhos exigem RightCtrl + tecla
-input_menu_toggle   = "f1"      # abre menu: RCTRL + F1
-input_exit_emulator = "escape"  # sair:     RCTRL + Esc
-input_save_state    = "f2"      # salvar:   RCTRL + F2
-input_load_state    = "f4"      # carregar: RCTRL + F4
-INI
-
-echo "fin"
+emulationstation --restart
